596ae5053b2b701dbc81b46424c8ea38fc0870c1
Mohamoud <mosd2004@hotmail.com>
2019-08-26 00:41:55 -0400

    finished all template

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+
+
 ]
 
 MIDDLEWARE = [
